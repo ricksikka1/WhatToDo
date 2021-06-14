a0a247ba7740a61364d7efc095778e370db52ddf
Raghav Sikka <ricksikka1@gmail.com>
2021-06-13 21:15:36 -0400

    GRPC server

---
+++ pkg/cmd/server.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+
+	// postgres driver
+	_ "github.com/lib/pq"
+
+	"github.com/ricksikka1/WhatToDo/pkg/protocol/grpc"
+	v1 "github.com/ricksikka1/WhatToDo/pkg/service/v1"
+)
+
+// Config is configuration for Server
+type Config struct {
+	// gRPC server start parameters section
+	// gRPC is TCP port to listen by gRPC server
+	GRPCPort string
+
+	// DB Datastore parameters section
+	// DatastoreDBHost is host of database
+	DatastoreDBHost string
+	// DatastoreDBUser is username to connect to database
+	DatastoreDBUser string
+	// DatastoreDBPassword password to connect to database
+	DatastoreDBPassword string
+	// DatastoreDBSchema is schema of database
+	DatastoreDBSchema string
+}
+
+// RunServer runs gRPC server and HTTP gateway
+func RunServer() error {
+	ctx := context.Background()
+
+	// get configuration
+	var cfg Config
+	flag.StringVar(&cfg.GRPCPort, "grpc-port", "", "gRPC port to bind")
+	flag.StringVar(&cfg.DatastoreDBHost, "db-host", "", "Database host")
+	flag.StringVar(&cfg.DatastoreDBUser, "db-user", "", "Database user")
+	flag.StringVar(&cfg.DatastoreDBPassword, "db-password", "", "Database password")
+	flag.StringVar(&cfg.DatastoreDBSchema, "db-schema", "", "Database schema")
+	flag.Parse()
+
+	if len(cfg.GRPCPort) == 0 {
+		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
+	}
+
+	connStr := "user=postgres dbname=postgres password=secure-password host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return fmt.Errorf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	v1API := v1.NewToDoServiceServer(db)
+
+	return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
+}
